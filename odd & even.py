7a08957262ea62e9c246d5441eafa7df37630f8e
ebinezh <88050562+ebinezh@users.noreply.github.com>
2021-08-03 21:53:48 +0530

    Add files via upload

---
+++ odd & even.py
@@ -0,0 +1,14 @@
+# to print even numbers
+n=int(input("Enter number: "))
+x=[i for i in range(1, n+1) if not i% 2]
+print("Even numbers till",n, "are", x)
+
+
+# to print odd numbers
+n=int(input("Enter number: "))
+x=[i for i in range(1, n+1) if i% 2]
+print("Odd numbers till",n, "are", x)
+
+# thanks for watching
+# Like, share & subscribe
+# Dream 2 code
